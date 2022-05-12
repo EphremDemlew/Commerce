86998105203570c0765b8dc035547878a7e0a07d
EphremDemlew <ephyaabb@gmail.com>
2022-05-12 09:03:49 +0300

    Login and register finished

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const route = express.Router();
 const User = require("../models/users");
-const { registerValidation } = require("../validation/validation");
+const {
+  registerValidation,
+  loginValidation,
+} = require("../validation/validation");
+const bcrypt = require("bcryptjs");
 
 route.post("/register", async (req, res) => {
   const { error } = registerValidation(req.body);
@@ -11,18 +15,42 @@ route.post("/register", async (req, res) => {
     return res.status(400).send("User alrady registered.");
   }
 
+  //encript(hash) the password
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
 
   try {
     const saveUser = await user.save();
-    res.status(200).send();
+    res.status(200).send(`User :${user._id}`);
   } catch (err) {
     res.status(400).send(err);
   }
+});
+
+route.post("/login", async (req, res) => {
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //check Email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return res.status(400).send("Email or Password is incorrect.");
+  }
+  //check Password
+  const correctPassword = await bcrypt.compare(
+    req.body.password,
+    user.password
+  );
+  if (!correctPassword) {
+    return res.status(400).send("Email or Password is incorrect.");
+  }
+  res.send(user._id);
 });
 
 module.exports = route;
